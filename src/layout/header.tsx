919b6357a2971d5a846166bb5e542226e6ab83cd
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2023-02-11 02:51:13 +0700

    Update header.tsx

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -11,14 +11,17 @@ export default function Header(props: Props) {
 
   useEffect(() => {
     commit({ type: "CHANGE_TITLE", payload: props.title ?? "Beranda" });
-  }, []);
+  }, [props.title]);
 
   return (
     <Head>
       <title>
-        {props.title} | {process.env.NEXT_PUBLIC_APP_NAME}
+        {state.title} | {process.env.NEXT_PUBLIC_APP_NAME}
       </title>
       <meta name="description" content="Aplikasi Sistem Informasi Desa" />
+      <meta name="keywords" content="Sistem Informasi Desa" />
+      <meta name="author" content="Alfad Sabil Haq" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/assets/images/logo.png" />
     </Head>
   )
